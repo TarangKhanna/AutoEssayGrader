81f8cc9aec293e2a8da63a2c7a242d0cc72a905b
Atul Aneja <atul.aneja75@gmail.com>
2017-10-06 11:27:01 -0400

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -13,11 +13,11 @@ from nltk.tokenize import word_tokenize
 from enchant.checker import SpellChecker
 
 
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
-result = firebase.get('/users', None)
-print result
-{'1': 'John Doe', '2': 'Jane Doe'}
+# from firebase import firebase
+# firebase = firebase.FirebaseApplication('https://your_storage.firebaseio.com', None)
+# result = firebase.get('/users', None)
+# print result
+# {'1': 'John Doe', '2': 'Jane Doe'}
 
 
 def login(request):
