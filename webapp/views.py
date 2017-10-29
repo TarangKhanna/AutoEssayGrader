a19792f65b176520a6748900471f99f670b39513
Shivank <shivanktibrewal@gmail.com>
2017-10-29 16:07:29 -0400

    Implemented grammar issues with context rendering

--- webapp/views.py
+++ webapp/views.py
@@ -16,6 +16,10 @@ from .firebase_util import auth, db, firebase
 
 import json
 
+import language_check
+
+from nltk import tokenize
+
 def signup(request):
 
     if request.method == 'POST':
@@ -96,17 +100,15 @@ def view_past_essays(request):
             user_email = request.session['user']['email']
             user_id_token = request.session['user']['idToken']
             all_essays_by_email = db.child("user_essay").order_by_child("email").equal_to(user_email).get()
-            print("\n\nAll Essays By Email: ")
+            #print("\n\nAll Essays By Email: ")
             for essay in all_essays_by_email.each():
-                #print(str("\nKey: " + essay.key()))
-                #print(str("Value: " + str(essay.val())))
                 essay_list.append(essay.val())
         except Exception as e:
                 error_json = e.args[1]
                 error = "Error: " + json.loads(error_json)['error']['message']
                 essay_list = [] 
 
-        print(str(essay_list))
+        #print(str(essay_list))
 
         return render_to_populated_response('past_essays.html',\
         {'title':"Auto Essay Grader",\
@@ -177,6 +179,9 @@ def handle_uploaded_essay(essay_file, essay_title, save_to_db,request):
     spelling_error_count = 0
     stop_word_count = 0
     spelling_errors = []
+    grammar_issues_list = []
+    grammar_issues_count = 0
+
     for chunk in essay_file.chunks():
         essay += chunk.decode("utf-8")
     try:
@@ -185,8 +190,8 @@ def handle_uploaded_essay(essay_file, essay_title, save_to_db,request):
         word_count = get_word_count(essay)
         stop_word_count = get_stop_word_count(essay)
         spelling_error_count, spelling_errors = get_spelling_error_count(essay)
-
-        print("Essay Grade: " + str(essay_grade) + " %")
+        grammar_issues_list, grammar_issues_count = get_grammer_correction_list(essay)
+        #print("Essay Grade: " + str(essay_grade) + " %")
 
         if (save_to_db):
             save_essay_to_db(essay, essay_grade, essay_title, request)
@@ -196,7 +201,21 @@ def handle_uploaded_essay(essay_file, essay_title, save_to_db,request):
         traceback.print_exc()
         essay_grade = "Couldn't be determined.."
 
-    return essay_grade,essay,word_count,stop_word_count,spelling_error_count, spelling_errors
+    return essay_grade,essay,word_count,stop_word_count,spelling_error_count, spelling_errors, grammar_issues_list, grammar_issues_count
+
+def get_grammer_correction_list(essay):
+
+    tool = language_check.LanguageTool('en-US')
+
+    matches = tool.check(essay)
+
+    grammar_issues = []
+
+    for match in matches:
+        if match.ruleId != 'MORFOLOGIK_RULE_EN_US': #Ignore spelling errors because another function is handling that
+            grammar_issues.append(str(match))
+
+    return grammar_issues, len(grammar_issues)
 
 def get_word_count(essay):
     return len(essay.split(" "))
@@ -205,7 +224,7 @@ def get_stop_word_count(essay):
     stop_words = set(stopwords.words('english'))
     word_tokens = word_tokenize(essay.lower())
     stop_word_count = len([w for w in word_tokens if w in stop_words])
-    print("stop words: " + str(stop_words))
+    #print("stop words: " + str(stop_words))
     return stop_word_count
 
 def get_spelling_error_count(essay):
@@ -228,7 +247,9 @@ def submit_essay(request):
             if form.is_valid():
                 essay_title = form.cleaned_data["title"]
                 save_to_db = form.cleaned_data["save_essay_checkbox"]
-                essay_grade,essay,word_count,stop_word_count,spelling_error_count, spelling_errors = handle_uploaded_essay(request.FILES['file'], essay_title, save_to_db,request)
+                essay_grade,essay,word_count,stop_word_count,spelling_error_count, spelling_errors, grammar_issues_list, grammar_issues_count = handle_uploaded_essay(request.FILES['file'], essay_title, save_to_db,request)
+                #for issue in grammar_issues_list:
+                #    print(str(issue))
                 return render_to_populated_response('upload_essay.html',\
                 {'title':"Auto Essay Grader",\
                 'user_name': user_name,\
@@ -238,7 +259,9 @@ def submit_essay(request):
                 'word_count':word_count,\
                 'spelling_error_count':spelling_error_count,\
                 'stop_word_count':stop_word_count,\
-                'spelling_errors':spelling_errors},request)
+                'spelling_errors':spelling_errors,\
+                'grammar_issues_list':grammar_issues_list,\
+                'grammar_issues_count':grammar_issues_count},request)
             else:
                 #This will return the same form but with errors displayed to the user
                 return render_to_populated_response('upload_essay.html',\
